d3d04f4f64542cdde365e14f13cd5f30aaccd959
Simon Van Braeckel <simonvanbraeckel@gmail.com>
2024-12-07 00:56:45 +0100

    test2

--- engines/query-sparql-rdfjs-lite/webpack.config.ts
+++ engines/query-sparql-rdfjs-lite/webpack.config.ts
@@ -3,8 +3,8 @@ import { createConfig } from '../../webpack.config';
 const liteConfig = createConfig(__dirname);
 
 if (typeof liteConfig.performance === 'object') {
-  liteConfig.performance.maxAssetSize = 900_100;
-  liteConfig.performance.maxEntrypointSize = 900_100;
+  liteConfig.performance.maxAssetSize = 900_200;
+  liteConfig.performance.maxEntrypointSize = 900_200;
 }
 
 export default liteConfig;
